4362199b36262231f253669530f7ecc0f62d2456
Júlia Saboya <juliasaboyaengenharia@gmail.com>
2024-04-10 07:38:35 -0300

    gambiarra

---
+++ Projeto_Carros/src/CarManager.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class CarManager {
+    private RoadPanel roadPanel;
+    private ArrayList<Car> leftCars;
+    private ArrayList<Car> rightCars;
+    private final int maxCars = 10;
+    public int leftCarX = 2;
+    public int rightCarX = 1080;
+    public int leftCarY = 300;
+    public int rightCarY = 410;
+    private Interface instance;
+
+    public CarManager(RoadPanel roadPanel, ArrayList<Car> leftCars, ArrayList<Car> rightCars) {
+        this.roadPanel = roadPanel;
+        this.leftCars = leftCars;
+        this.rightCars = rightCars;
+    }
+
+    public void createCar(boolean left, int identifier, int crossingTime, int waitingTime) {
+        if (left) {
+            if (leftCars.size() < maxCars) {
+                Car newCar = new Car(leftCarX, leftCarY, identifier, crossingTime, waitingTime, instance, true);
+                leftCars.add(newCar);
+                roadPanel.repaint();
+            } else {
+                JOptionPane.showMessageDialog(null, "Limite de carros à esquerda atingido!");
+            }
+        } else {
+            if (rightCars.size() < maxCars) {
+                Car newCar = new Car(rightCarX, rightCarY, identifier, crossingTime, waitingTime, instance, false);
+                rightCars.add(newCar);
+                roadPanel.repaint();
+            } else {
+                JOptionPane.showMessageDialog(null, "Limite de carros à direita atingido!");
+            }
+        }
+    }
+
+    public void updateCarsPosition() {
+        for (Car car : leftCars) {
+            car.move(); // Move os carros para a esquerda
+        }
+        for (Car car : rightCars) {
+            car.move(); // Move os carros para a direita
+        }
+        roadPanel.repaint();
+    }
+}
+
+
+
